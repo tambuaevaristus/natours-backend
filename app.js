139ba65fb906b115e9c3a01a9b3382824cd6ee89
Tambua Evaristus <evaristustambua@gmail.com>
2022-09-16 18:02:52 +0100

    middlewares

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
 const express = require("express");
 const { application } = require("express");
+const morgan = require("morgan");
 
 const app = express();
-const port = 3000;
 
 // Middleware
+app.use(morgan("tiny"));
+
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log("Hello from middleware");
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -93,20 +99,109 @@ const deleteTour = (req, res) => {
   });
 };
 
-// // app.get("/api/v1/tours", getAllTours);
-// app.get("/api/v1/tours/:id", getTour);
-// app.post("/api/v1/tours", createTour);
-// app.patch("/api/v1/tours/:id", updateTour);
-// app.delete("/api/v1/tours/:id", deleteTour);
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
 
-app.route("api/v1/tours").get(getAllTours).post(createTour);
+const getUser = (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
 
-app
-  .route("/api/v1/tours/:id")
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: "not_found",
+      message: "invalid id",
+    });
+  }
+  res.status(200).json({
+    status: "ok",
+    data: {
+      tour,
+    },
+  });
+};
+const createUser = (req, res) => {
+  console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "successfull",
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+};
+
+const updateUser = (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: "ivalid id here",
+      message: "invalid id",
+    });
+  }
+  res.status(200).json({
+    data: {
+      tour: "<Updated tour >",
+    },
+  });
+};
+
+const deleteUser = (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: "ivalid id here",
+      message: "invalid id",
+    });
+  }
+  res.status(204).json({
+    data: null,
+  });
+};
+
+const tourRouter = express.Router();
+const userRouter = express.Router();
+app.use('/api/v1/tours', tourRouter)
+tourRouter.route("/").get(getAllTours).post(createTour);
+
+tourRouter
+  .route("/:id")
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
 
+
+app.use('/api/v1/users', userRouter)
+userRouter.route("/").get(getAllUsers).post(createUser);
+
+userRouter
+  .route("/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
