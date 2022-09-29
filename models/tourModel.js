a2c93b9dea01900ab3f0742836c51a0176adfee4
Tambua Evaristus <evaristustambua@gmail.com>
2022-09-29 07:54:55 +0100

    schema changed

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,6 +5,27 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Tour must have a in name'],
     unique: true,
+    trim: true,
+  },
+  duration:{
+    type: Number,
+    required: [true, 'Tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a max group size'],
+  },
+  difficulty:{
+    type: String,
+    required: [true, 'Tour must have a dificuty'],
+  },
+  ratingsAverage:{
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuatity:{
+    type: Number,
+    default: 0
   },
   rating: {
     type: Number,
@@ -14,6 +35,26 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'Tour must have a price'],
   },
+  priceDiscount:Number,
+  summary:{
+    type: String,
+    trim: true
+  }, 
+  description:{
+    type: String,
+    trim: true,
+  },
+  imageCover:{
+    type:String,
+    required: [true, 'Image must bbe present']
+  },
+  image: [String],
+
+  createdAt: {
+    type:Date,
+    default: Date.now()
+  },
+  startDate:[Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
